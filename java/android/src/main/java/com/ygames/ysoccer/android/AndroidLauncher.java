f36d4c75ea51d02fb28f2015020e98f792873b24
Massimo Modica <massimo.modica@gmail.com>
2025-02-07 19:26:35 +0100

    gdxVersion upgraded to 1.13.1

---
+++ java/android/src/main/java/com/ygames/ysoccer/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package com.ygames.ysoccer.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.ygames.ysoccer.YSoccer;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new YSoccer(), configuration);
+    }
+}
