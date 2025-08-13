cbb992879960bcde4f65b3f4e656d7dc159465dd
Massimo Modica <massimo.modica@gmail.com>
2025-08-13 17:16:04 +0200

    New class DataDto

--- java/core/src/main/java/com/ygames/ysoccer/network/dto/BallDto.java
+++ java/core/src/main/java/com/ygames/ysoccer/network/dto/BallDto.java
@@ -1,9 +1,11 @@
 package com.ygames.ysoccer.network.dto;
 
 public class BallDto {
+
     public float x;
     public float y;
     public float z;
+    public DataDto currentDataDto;
 
     public BallDto() {
     }
--- java/core/src/main/java/com/ygames/ysoccer/network/dto/PlayerDto.java
+++ java/core/src/main/java/com/ygames/ysoccer/network/dto/PlayerDto.java
@@ -1,7 +1,9 @@
 package com.ygames.ysoccer.network.dto;
 
 public class PlayerDto {
+
     public String name;
+    public DataDto currentDataDto;
 
     public PlayerDto() {
     }
