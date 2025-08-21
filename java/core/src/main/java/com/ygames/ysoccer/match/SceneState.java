485ca59be6886f65da839ee89f9485c30bc7bcb0
Massimo Modica <massimo.modica@gmail.com>
2025-08-21 18:35:48 +0200

    Restore call to setDisplayFlags() in entryActions()

--- java/core/src/main/java/com/ygames/ysoccer/match/SceneState.java
+++ java/core/src/main/java/com/ygames/ysoccer/match/SceneState.java
@@ -24,6 +24,7 @@ abstract class SceneState<SceneFsmT extends SceneFsm<SceneT, ?>, SceneT extends 
 
     void entryActions() {
         timer = 0;
+        setDisplayFlags();
     }
 
     void exitActions() {
